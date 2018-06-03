ff9bba31940820aadf800777e959988c496f5518
touchface <572165213@qq.com>
2018-06-03 22:34:05 +0800

    完成了与格子与格子形状相关的基本类

---
+++ app/src/main/java/top/touchface/tetris/Cell.java
@@ -0,0 +1,56 @@
+package top.touchface.tetris;
+/**
+ * @author Jason
+ * 格子类，用于构成俄罗斯方块的基本形状
+ * **/
+public class Cell implements Cloneable{
+
+    private int x;
+    private int y;
+
+    public Cell(){
+        x=0;
+        y=0;
+    }
+    /**
+     *格子的构造方法，用于初始化格子对象
+     *@param x 格子的横坐标
+     *@param y 格子的纵坐标
+     * **/
+    public Cell(int x, int y) {
+        this();
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void move(int x,int y){
+        this.x+=x;
+        this.y+=y;
+    }
+    @Override
+    public Object clone(){
+        Cell cell=null;
+        try {
+            cell=(Cell) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return  cell;
+    }
+
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
---
+++ app/src/main/java/top/touchface/tetris/Tetromino.java
@@ -0,0 +1,123 @@
+package top.touchface.tetris;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @author Jason
+ * 俄罗斯方块的基本形状类
+ * **/
+public class Tetromino {
+    //由4个方块构成的基本形状
+    private Cell cells[];
+    //当前形状的颜色
+    private int color;
+    //当前形状的类型
+    private int type;
+    public Tetromino(Cell cells[],int color,int type){
+        this.cells=cells;
+        this.color=color;
+        this.type=type;
+    }
+
+    public Cell[] getCells() {
+        return cells;
+    }
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    /**
+     *移动当前的形状
+     * **/
+    public void move(int x,int y){
+        for (Cell cell:cells){
+            cell.move(x,y);
+        }
+    }
+    public static Tetromino newRandomInstance(){
+        //七种颜色值
+        int ranColor[]={0xFFFF0000,0xFFFF7F00,0xFFFFFF00,0xFF00FF00,0xFF00FFFF,0xFF0000FF,0xFF8B00FF};
+
+        Random random=new Random();
+        int type=random.nextInt(7);//生成类型
+        int color=random.nextInt(ranColor.length);//生成随机颜色
+        Cell cells[]=null;
+        switch (type) {
+            case 0:
+                //田
+                cells = new Cell[]{
+                        new Cell(4, 0),
+                        new Cell(5, 0),
+                        new Cell(4, 1),
+                        new Cell(5, 1)};
+                break;
+            case 1:
+                //L
+                cells = new Cell[]{
+                        new Cell(4, 1),
+                        new Cell(3, 0),
+                        new Cell(3, 1),
+                        new Cell(5, 1)};
+                break;
+            case 2:
+                //反L
+                cells = new Cell[]{
+                        new Cell(4, 1),
+                        new Cell(5, 0),
+                        new Cell(3, 1),
+                        new Cell(5, 1)};
+                break;
+            case 3:
+                //——
+                cells = new Cell[]{
+                        new Cell(5, 0),
+                        new Cell(3, 0),
+                        new Cell(4, 0),
+                        new Cell(6, 0)};
+                break;
+            case 4:
+                //Z
+                cells = new Cell[]{
+                        new Cell(4, 1),
+                        new Cell(3, 0),
+                        new Cell(4, 0),
+                        new Cell(5, 1)};
+                break;
+            case 5:
+                //反Z
+                cells = new Cell[]{
+                        new Cell(4, 1),
+                        new Cell(3, 1),
+                        new Cell(4, 0),
+                        new Cell(5, 0)};
+                break;
+            case 6:
+                //凸
+                cells = new Cell[]{
+                        new Cell(4, 1),
+                        new Cell(3, 1),
+                        new Cell(4, 0),
+                        new Cell(5, 1)};
+                break;
+        }
+        Tetromino tetromino=new Tetromino(cells,color,type);
+        return tetromino;
+    }
+
+    @Override
+    public String toString() {
+        return "Tetromino{" +
+                "cells=" + Arrays.toString(cells) +
+                ", color=" + color +
+                ", type=" + type +
+                '}';
+    }
+}
