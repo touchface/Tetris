b0a0455596b3fd9f44c97200685e66676233c7eb
touchface <572165213@qq.com>
2018-06-03 23:05:18 +0800

    添加属性的set方法

--- app/src/main/java/top/touchface/tetris/Cell.java
+++ app/src/main/java/top/touchface/tetris/Cell.java
@@ -31,7 +31,15 @@ public class Cell implements Cloneable{
         return y;
     }
 
-    public void move(int x,int y){
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void move(int x, int y){
         this.x+=x;
         this.y+=y;
     }
