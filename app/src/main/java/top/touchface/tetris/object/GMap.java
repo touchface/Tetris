047db3b4364ac06ecbcbc3532017aabb126ce81f
touchface <572165213@qq.com>
2018-06-04 09:33:22 +0800

    完成游戏地图类 实现cleanGmap函数

---
+++ app/src/main/java/top/touchface/tetris/object/GMap.java
@@ -0,0 +1,27 @@
+package top.touchface.tetris.object;
+
+import top.touchface.tetris.object.Cell;
+
+/**
+ * @author Jason
+ * 游戏地图类，用于保存堆积的方块
+ * **/
+public class GMap {
+    public Cell cells[][];
+    public GMap(int map_x, int map_y){
+        cells=new Cell[map_x][map_y];
+    }
+    /**
+     * 清空游戏地图类
+     * **/
+    public void cleanGmap(){
+        if(cells==null){
+            return;
+        }
+        for (int x=0;x<cells.length;x++){
+            for (int y=0;y<cells.length;y++){
+                cells[x][y]=null;
+            }
+        }
+    }
+}
